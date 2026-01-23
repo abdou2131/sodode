ae3616b919b3dab270f4347566a662695a1e10e9
abdou2131 <saam2131@gmail.com>
2026-01-23 12:26:01 +0100

    changement abdou1

--- conditions.py
+++ conditions.py
@@ -1,8 +1,19 @@
-name = "mayar"
-age = 10
-if (age < 18) :
+# name = "mayar"
+# age = 10
+# if (age < 18) :
     
-    print ("Mineur")
+#     print ("Mineur")
 
+# else :
+#     print (adulte)
+
+username_pred = "abdou2131"
+passward_pred = "123A2131a" 
+ 
+username = input("enter your username" )
+passward = input ("entre your passward")
+
+if (username == username_pred and passward == passward_pred):
+    print ("Information correctes," + username_pred )
 else :
-    print (adulte)
+    print ("information incorrecte")
