3cbe5c395752d622ab12cf3d744d5d810ab6dfac
abdou2131 <saam2131@gmail.com>
2026-02-04 15:56:02 +0100

    fonction

---
+++ Fonction_def.py
@@ -0,0 +1,35 @@
+# ex 01
+
+# def convert_temperatur(C):
+#     F=(C*9/5)+32
+#     return F
+# print (convert_temperatur(0))
+# print (convert_temperatur(32))
+
+
+# ex02
+
+# def sum_all(*numbers):
+#     return sum(numbers)
+# print(sum_all(1,2,3,4,5))
+
+# ex03
+
+# def display_info(**info):
+
+
+# ex04
+
+
+def find_maximum(*numbers):
+    return max(numbers)
+
+print(find_maximum(5,8,12,93,105))
+
+
+
+
+
+
+
+
