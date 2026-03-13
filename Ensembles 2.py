05efa27e961f60318bb9348bfd9b49bfbb48704e
abdou2131 <saam2131@gmail.com>
2026-03-13 12:01:26 +0100

    new

---
+++ Ensembles 2.py
@@ -0,0 +1,45 @@
+# A={1,2,3,4,5}
+# B={4,5,6,7,8}
+# # methodes 1
+# print(A |B)
+# print(A&B)
+# print(A-B)
+
+# # methodes 1
+
+# print(A.union (B))
+# print(A.intersection (B))
+# print(A. difference (B))
+
+# exercice 1
+
+# numbers=[1,2,2,3,4,4,5,6,6]
+# ensemble =set(numbers)
+# print(ensemble)
+# print(len(ensemble))
+
+# corction ex 1
+# numbers=[1,2,2,3,4,4,5,6,6]
+# ensemble =set(numbers)
+# numbers_final = list(ensemble)
+# print(numbers_final)
+# print(len(numbers_final))
+
+
+
+# exercice 2
+
+
+A={1,2,3,4}
+B={3,4,5,6}
+
+# methodes 1
+print(A | B)
+print(A & B)
+print(A - B)
+
+# methodes 1
+
+print(A.union (B))
+print(A.intersection (B))
+print(A. difference (B))
---
+++ tuple 2 .py
@@ -0,0 +1,103 @@
+# personne = (12 ,13, "ALI")
+# print(personne)
+
+# etap 1
+
+# t=(10,20,30,40)
+# print(t[3])
+# print (t[-2])
+
+# etap2 découpage 
+
+# t=(10,20,30,40, 50 , 60)
+# print(t[1:4])
+
+# etape 3 opérations sur les tuples
+# 1
+# t=(1,2,3,4,5,) 
+# print(len(t))
+
+
+# 2
+# t=(1,2,3,4,5,) 
+# p=(6,7,8)
+# k= t+p
+
+# Exerrcice 1 
+
+# numbers = (5,10,15,20,25,30)
+
+# print("premier élément : ", numbers[0])
+# print("Dernir élément : ", numbers[-1])
+# print("somme : ",sum(numbers))
+# print("longueur : " , len(numbers))
+
+# # coriction ex1
+
+
+# numbers = (5,10,15,20,25,30)
+
+# print("premier élément : ", numbers[0])
+# print("Dernir élément : ", numbers[-1])
+
+
+# # metode 1
+# summ = sum(numbers)
+# print("somme : " , summ)
+
+
+# # metode 2
+# sum = 0
+# for i in range(len (numbers)) :
+#     sum=sum+numbers[i]
+# print("somme : ",sum)
+
+
+# # metode 3
+# sum2= 0
+# for number in numbers :
+#     sum2 =sum2 +number
+# print("somme2 : ",sum2)
+
+
+# print("longueur : " , len(numbers))
+
+
+
+# Exerrcice 2 
+
+# products = (
+#     ("Mouse" , 25) ,
+#     ("keyboard" , 80) ,
+#     ("Laptop" , 1200) ,
+#     ("monitor" , 300)
+# ) 
+
+# for product in products : 
+#     print(product[0])
+
+# for product in products : 
+#     print(product[1])
+
+# max= products[0]
+# for i in range (len(products)) :
+#     if(i ==len(products)-1 ):
+#         break
+#     if(max [1]<= products[i+1][1]):
+#         max= products[i+1]
+# print (max)
+
+
+# Exerrcice 2 1
+
+# list = [1200,25,80,300,1900]
+# max2= list[0]
+# for i in range(len(list)) : 
+#     if(i==len(list)-1):
+#         break
+#     if(max2<=list[i+1]):
+#         max2=list[i+1]
+# print(max2)
+
+
+
